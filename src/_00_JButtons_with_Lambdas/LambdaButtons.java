7c808b1ec409a47495757c510bb31bb45493d930
League Student <league@WorthyMini-6.attlocal.net>
2019-05-19 17:32:53 -0700

    Finished Module 00, Began Module 01

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,11 +1,13 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class LambdaButtons {
+public class LambdaButtons{
 	private JFrame window = new JFrame();
 	private JButton addNumbers = new JButton("ADD 2 Numbers");
 	private JButton randNumber = new JButton("RANDOM NUMBER");
@@ -20,6 +22,13 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
+		addNumbers.addActionListener(e -> System.out.println("1 + 1 = 2"));
+		randNumber.addActionListener(e -> System.out.println((new Random().nextInt(10) + 1)));
+		tellAJoke.addActionListener(e -> System.out.println("Three programmers walk into a bar. \n"
+				+ "The bartender asks, \"Y\'all having beer?\"\n"
+				+ "\"I don't know,\" says the first.\n"
+				+ "\"I don't know,\" says the second.\n"
+				+ "\"Yes,\" says the third."));
 		
 		window.setVisible(true);
 		window.pack();
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -1,5 +1,8 @@
 package _01_Lambda_Methods;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
@@ -13,12 +16,39 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
+		printCustomMessage(s -> System.out.println(new StringBuilder(s).reverse().toString()), "backwards");
 		
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
+		printCustomMessage(s -> {
+			char[] sArray = s.toCharArray();
+			StringBuilder str = new StringBuilder();
+			for(char c : sArray) {
+				if(new Random().nextBoolean())
+					str.append(("" + c).toUpperCase());
+				else
+					str.append(("" + c).toLowerCase());
+			}
+			System.out.println(str.toString());
+		}, "capitalize");
 		
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
+		printCustomMessage(s -> {
+			StringBuilder str = new StringBuilder(s);
+			int finalLength = str.length() * 2 - 1;
+			for(int i = 1; i < finalLength; i += 2) {
+				str.insert(i, ".");
+			}
+			System.out.println(str.toString());
+		}, "periods");
 		
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
+		printCustomMessage(s -> {
+			StringBuilder str = new StringBuilder(s);
+			char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u'};
+			// How can I use a stream to complete this?
+			// Will the stream apply to vowels or str?
+		}, "vowels");
+		
 	
 	}
 	
